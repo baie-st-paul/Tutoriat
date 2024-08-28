2350f92371b1f9714379c0aec71ec8ce56e29306
Philippe Vallerand <pvallerand1@gmail.com>
2024-08-27 22:21:32 -0400

    init sql into sql files for best practice

---
+++ src/main/SQL/init_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (email, first_name, last_name, password)
+    VALUES ('jeff@teacher.com', 'jeff', 'log', 'root');
+
+INSERT INTO users (email, first_name, last_name, password)
+    VALUES ('sam@student.com', 'sam', 'bozo', 'root');
---
+++ src/main/SQL/init_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
